d9488050a1a6d303fa955b87e34ac9e5b6db754b
Rodrigo Freitas <rodrigo.santos203@fatec.sp.gov.br>
2019-07-21 21:29:48 -0300

    editando as classes

--- Pedido/Entities/OrderItem.cs
+++ Pedido/Entities/OrderItem.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Pedido.Entities {
     class OrderItem {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product product { get; set; } = new Product();
+
+        public OrderItem() { }
+
+        public OrderItem(int quantity,double price, Product orderProduct) {
+            Quantity = quantity;
+            Price = price;
+            product = orderProduct;
+        }
+
+        public double SubTotal() {
+            return Price * Quantity;
+        }
     }
 }
